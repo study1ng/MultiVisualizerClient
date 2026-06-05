ac66a4b5a21f297b80646a18e48ee53722527d5e
GBL0101 <reor97268@gmail.com>
2026-06-05 13:45:14 +0900

    revise .gitignore

---
+++ src/env.tsx
@@ -0,0 +1,9 @@
+declare global {
+    var base_filename: string;
+    var fn_filenames: string[];
+    var gt_filename: string;
+}
+
+globalThis.base_filename = "/home/skitazawa/MultiVisualizerClient/.teststatic/99_533_image.nii.gz";
+globalThis.gt_filename = "/home/skitazawa/MultiVisualizerClient/.teststatic/99_533_gt.nii.gz";
+globalThis.fn_filenames = ["/home/skitazawa/MultiVisualizerClient/.teststatic/99_533_out.nii.gz"];
